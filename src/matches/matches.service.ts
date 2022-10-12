9155af68596ea23809a9f8df6f58c4c8789a2238
Khauam-Kleber <khauam@hotmail.com>
2022-10-12 16:18:57 -0300

    deploy junto com o front

--- src/matches/matches.service.ts
+++ src/matches/matches.service.ts
@@ -6,32 +6,39 @@ import { Match, MatchDocument } from './entities/match.entity';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class MatchesService  extends BaseService<Match>{
+export class MatchesService extends BaseService<Match>{
   constructor(
-    @InjectModel(Match.name) private matchModel: Model<MatchDocument>,   private usersService: UsersService,
-  ) { 
+    @InjectModel(Match.name) private matchModel: Model<MatchDocument>, private usersService: UsersService,
+  ) {
     super(matchModel);
   }
 
   async findAll() {
     let user = await this.usersService.getRequestUser();
-    return this.matchModel.find({ teamHome : user.team._id.toString() }).populate('teamAgainst'); //.exec(); 
+    if (user.team) {
+      return this.matchModel.find({ teamHome: user.team._id.toString() }).populate('teamAgainst'); //.exec(); 
+    } else {
+      return [];
+    }
     // return this.matchModel.find().populate('teamAgainst'); //.exec(); 
 
   }
 
-  async findDashboardInfos(){
+  async findDashboardInfos() {
+    let user = await this.usersService.getRequestUser();
     let infoList = []
-    infoList.push({"value" : await this.countAllWinMatches(), name: "Partidas Ganhas"})
-    infoList.push({"value" : await this.countAllLoseMatches(), name: "Partidas Perdidas" })
+    if (user.team) {
+      infoList.push({ "value": await this.countAllWinMatches(user.team._id.toString()), name: "Partidas Ganhas" })
+      infoList.push({ "value": await this.countAllLoseMatches(user.team._id.toString()), name: "Partidas Perdidas" })
+    }
     return infoList;
   }
 
-  async countAllWinMatches() {
-    return this.matchModel.count({ $expr: { $gt: [ "$scoreboardTeamHome" , "$scoreboardTeamAgainst" ] }});
+  async countAllWinMatches(userId) {
+    return this.matchModel.count({ teamHome: userId, $expr: { $gt: ["$scoreboardTeamHome", "$scoreboardTeamAgainst"] } });
   }
 
-  async countAllLoseMatches() {
-    return this.matchModel.count({ $expr: { $lt: [ "$scoreboardTeamHome" , "$scoreboardTeamAgainst" ] }});
+  async countAllLoseMatches(userId) {
+    return this.matchModel.count({ teamHome: userId, $expr: { $lt: ["$scoreboardTeamHome", "$scoreboardTeamAgainst"] } });
   }
 }
